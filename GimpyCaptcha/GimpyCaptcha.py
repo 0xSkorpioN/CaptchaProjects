b8c9c26560e94eb503ce177b8d63cf513886d2f7
Abdallah Adham <abdallahadham1998@gmail.com>
2022-02-02 04:04:53 +0200

    first commit

---
+++ GimpyCaptcha/GimpyCaptcha.py
@@ -0,0 +1,35 @@
+from captcha.image import ImageCaptcha  # pip install captcha
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import random
+
+Random = ['1','2','3','4','5','6','7','8','9','0']
+MAX_CAPTCHA = 6
+WIDTH=100
+HEIGHT=30
+
+image = ImageCaptcha(width=WIDTH, height=HEIGHT, font_sizes=[26])
+
+captcha_text = []
+for i in range(MAX_CAPTCHA):
+    c = random.choice(Random)
+    captcha_text.append(c)
+    print(captcha_text)
+captcha_text = ''.join(captcha_text)
+print(captcha_text)
+
+captcha = image.generate(captcha_text)
+captcha_image = Image.open(captcha)
+captcha_image = np.array(captcha_image)
+
+image.write(captcha_text, str(i)+'_'+captcha_text + '.png')
+plt.imshow(captcha_image)
+plt.show()
+
+print("Enter the Numbers shown in the figure : ")
+x = input()
+if x == captcha_text:
+    print ("Verified")
+if x != captcha_text:
+    print ("Wrong")
